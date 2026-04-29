a62d73b853cbb6491701f09172088fc84a70d881
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2026-04-29 11:25:51 +0800

    revert: plugin config for users-permissions

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,27 @@
-import type { Core } from '@strapi/strapi';
+// import type { Core } from '@strapi/strapi';
+//
+// const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+//
+// export default config;
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }) => ({
+  "users-permissions": {
+    config: {
+      jwt: {
+        expiresIn: "7d",
+      },
+      register: {
+        allowedFields: [
+          "name",
+          "username",
+          "email",
+          "password",
+          "userRole",
+          "eventId",
+        ],
+      },
+    },
+  },
+});
 
 export default config;
